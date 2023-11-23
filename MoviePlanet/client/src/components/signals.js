f3f5f628944e5d8a182f05d9de1b973082ec3aed
SiniKykyri <sini.kykyri@gmail.com>
2023-11-23 14:24:04 +0200

    Käyttäjän kirjautuminen lisätty
    Käyttäjän kirjautuminen reactin puolella lisätty. Painamalla kirjaudu nappia avautuu ikkuna, johon käyttäjä voi syöttää käyttäjätunnuksen sekä salasanan. Käyttäjä saa virheilmoituksen mikäli salasana tai käyttäjätunnus on väärin. Ikkunassa on ajastin, jos tiedot on syötetty väärin.

--- MoviePlanet/client/src/components/signals.js
+++ MoviePlanet/client/src/components/signals.js
@@ -1,3 +1,6 @@
 import {signal} from "@preact/signals-react";
 
+//Signaali kirjautumistokenille
 export const jwtToken = signal('');
+//Signaali onko kirjautumisikkuna auki vai ei
+export const LoginFormOpen = signal(false);
