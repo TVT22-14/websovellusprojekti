b563265ac5130e9eeda64c00969d7779892b67b6
embsu <emilia.kumpuniemi@gmail.com>
2023-11-24 13:17:36 +0200

    navbarin alustus
    linkit toimii

--- MoviePlanet/client/src/App.js
+++ MoviePlanet/client/src/App.js
@@ -2,15 +2,47 @@ import logo from './logo.svg';
 import './App.css';
 import { jwtToken, LoginFormOpen } from './components/signals';
 import { useState } from 'react';
-import {LoginForm, openModal} from './components/auth';
+
+
+// IMPORTIT ROUTE LINKEILLE
+import { BrowserRouter as Router, Route, Switch, Routes } from 'react-router-dom';
+import NavBar from './components/navbar';
+
+import FrontPageView from './components/frontpageview';
+import NewsView from './components/news';
+import AllMovies from './components/movies';
+import MakeReview from './components/review';
+import Communities from './components/communities';
+//ilmoitukset tähän
+//asetukset tähän
+//luokayttaja tähän
+import { LoginForm, openModal } from './components/auth';
+//___________________________________________________________
 
 function App() {
 
   return (
-    <div>
-      <button id='Login' onClick={openModal}>Kirjaudu sisään</button> {/* Kirjaudu sisään nappi, joka kutsuu openModal functiota auth.js tiedostosta */}
-      {LoginFormOpen.value == true && <LoginForm />} {/* Loginform komponentti renderöidään vain jos signaalin LoginFormOpen arvo on true */}
-    </div>
+
+    <Router>
+      <div>
+        <NavBar />
+        <Routes>
+          <Route path="/" exact element={<FrontPageView/>} />
+          <Route path="/uutiset" element={<NewsView/>} />
+          <Route path="/elokuvat" element={<AllMovies/>} />
+          <Route path="/arvostelut" element={<MakeReview/>} />
+          <Route path="/ryhmat" element={<Communities/>} />
+          {/*<Route path="/ilmoitukset" element={<xxxx/>} />  näkyy vain kirjautuneille */}
+          {/*<Route path="/asetukset" element={<xxxx/>} />  näkyy vain kirjautuneille */}
+          {/* <Route path="/luokayttaja" element={<xxxx/>} /> */}
+          <Route path="/kirjaudu" element={<LoginForm/>} />
+        </Routes>
+
+        {/* <NewsView /> */}
+      </div>
+
+    </Router>
+
   );
 }
 
--- MoviePlanet/client/src/components/settings.js
+++ MoviePlanet/client/src/components/settings.js
@@ -5,3 +5,14 @@
 
 // Omien ryhmien tietojen hakeminen
 // Kato UI suunnitelma sivu 5
+import React from 'react';
+
+function Settings() {
+    return(
+        <div>
+            <h1>settings</h1>
+        </div>
+    )
+    }
+
+    export default Settings;
