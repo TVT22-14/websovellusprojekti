aa701495f014517194a8b98317674ec0cb827a4c
Topi Ilmasti <122445154+SUPPlS@users.noreply.github.com>
2023-11-27 12:31:47 +0200

    kaikki endpointit valmiina, movies valm ja parsitt

--- MoviePlanet/postgre/review.js
+++ MoviePlanet/postgre/review.js
@@ -7,12 +7,13 @@ const sql = {
     GET_REVIEWS: 'SELECT idreview, review, movieidapi, moviestars, idcustomer FROM review',
     GET_MOVIES: 'SELECT movieidapi FROM review WHERE movieidapi = $1',
     DELETE_REVIEW: 'DELETE FROM review WHERE idreview = $1'
-}
+};
 
 // ADD REVIEW TO DATABASE
 async function addReview(review, movieidapi, moviestars, idcustomer) {
     await pgPool.query(sql.INSERT_REVIEW, [review, movieidapi, moviestars, idcustomer]);
 };
+
 // GET REVIEWS FROM DATABASE
 async function getReview() {
     const result = await pgPool.query(sql.GET_REVIEWS);
@@ -22,7 +23,11 @@ async function getReview() {
 };
 
 // GET MOVIES FROM DATABASE
-// async function getMovies() {};
+async function getMovies(movieidapi) {
+    const result = await pgPool.query(sql.GET_MOVIES, [movieidapi]);
+    const rows = result.rows;
+    return rows;  
+};
 
 // DELETE REVIEW FROM DATABASE
 async function deleteReview(idreview) {
@@ -32,11 +37,11 @@ async function deleteReview(idreview) {
             return { success: true, message: 'Review deleted successfully.' };
         } catch (error) {
     
-            await pgPool.query('ROLLBACK'); // Rollback the transaction if an error occurs
+            await pgPool.query('ROLLBACK');
             console.error('Error deleting review:', error);
             return { success: false, error: error.message };
         }    
 };
 
 // EXPORT FUNCTIONS
-module.exports = { addReview, getReview, deleteReview };     
+module.exports = { addReview, getReview, deleteReview, getMovies };
--- MoviePlanet/routes/review.js
+++ MoviePlanet/routes/review.js
@@ -1,8 +1,10 @@
+require('dotenv').config();
 const router = require('express').Router();
 const multer = require('multer');
 const upload = multer({ dest: 'uploads/' });
+const axios = require('axios');
 
-const { addReview, getReview, deleteReview } = require('../postgre/review');
+const { addReview, getReview, deleteReview, getMovies } = require('../postgre/review');
 
 // ADD REVIEW TO DATABASE
 router.post('/', upload.none(), async (req, res) => {
@@ -24,32 +26,56 @@ router.post('/', upload.none(), async (req, res) => {
 // GET REVIEWS FROM DATABASE
 router.get('/allmoviereviews', async (req, res) => {
     res.json(await getReview());
-})
-// GET MOVIES FROM DATABASE
-router.post('/', upload.none(), async (req, res) => {
-    const review = req.body.review;
-    const movieidapi = req.body.movieidapi;
-    const moviestars = req.body.moviestars;
-    const idcustomer = req.body.idcustomer;
+});
 
-    console.log(review, movieidapi, moviestars, idcustomer);
+// GET MOVIES FROM DATABASE
+router.get('/movies', async (req, res) => {
 
     try {
-        await addReview(review, movieidapi, moviestars, idcustomer);
-        res.end();
+        const response = await axios.get('https://api.themoviedb.org/3/search/movie', {
+        params: {
+        api_key: process.env.TMDB_API_KEY,
+        query: req.query.movieidapi,
+            },
+        });
+        console.log(response.data);
+        // Palauta ensimmäinen elokuva hakutuloksista
+        if (response.data.results.length > 0) {
+            const elokuva = response.data.results[0];
+            console.log(elokuva);
+
+            const parsittuElokuva = {
+                id: elokuva.id,
+                title: elokuva.title,
+                poster_path: elokuva.poster_path,
+            };
+
+            const moviesInDatabase = await getMovies(elokuva.id);
+            if (moviesInDatabase.length > 0) {
+                parsittuElokuva.isInDatabase = true;
+            } else {
+                parsittuElokuva.isInDatabase = false;
+            }
+
+            res.json(parsittuElokuva);
+        } else {
+            res.status(404).json({ message: 'Elokuvaa ei löytynyt' });
+        }
     } catch (error) {
-        res.json({ error: error.message }).status(500);
+        console.error(error);
+        res.status(500).json({ message: 'Virhe elokuvan hakemisessa' });
     }
 });
+
 // DELETE REVIEW FROM DATABASE
 router.delete('/', async (req, res) => {
     const idreview = req.query.idreview;
-    console.log("poistetaan elokuva", idreview);
+    console.log("Poistetaan elokuva", idreview);
     
     try {
         await deleteReview(idreview);
         res.end();
-        console.log("elokuva", idreview, "poistettu");
+        console.log("Elokuva", idreview, "poistettu");
     } catch (error) {
         res.json({ error: error.message }).status(500);
     }
